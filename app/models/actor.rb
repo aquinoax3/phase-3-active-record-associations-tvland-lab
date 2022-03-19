5419b5d611b04c35b08ea1aac7e05069180795c0
Axel Aquino <aquino.axel@gmail.com>
2022-03-19 17:52:50 -0400

    commit tree 455c0544b443d0c436ceabb7bbce63d115bcf1b7

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        # get all the characters
+        self.characters.map do {|character| "#{character.name} - #{character.show.name}"}
+    end
+
+    def a_star_is_born(first_name, last_name)
+        Actor.create(first_name:first_name, last_name:last_name).save
+    end
+
+
+
 end
